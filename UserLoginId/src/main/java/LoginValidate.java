152f7c52cb121d5ef909fdf7ff78f1a2d47cd2b0
ashokreddy22 <ashokkumarreddy2234@example.com>
2022-08-03 18:44:13 +0530

    changes

---
+++ UserLoginId/src/main/java/LoginValidate.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginValidate
+ */
+@WebServlet("/login")
+public class LoginValidate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginValidate() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	PrintWriter out=response.getWriter();
+	response.setContentType("text/html");
+	String name=request.getParameter("username");
+	String password=request.getParameter("Password");
+	if(name.equals("Abcd123")&&password.equals("12345678")) {
+		out.println("Login sussessfully into a Account");}
+		else {
+			out.println("Login failed...Invalid credentials");
+		}
+	
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
